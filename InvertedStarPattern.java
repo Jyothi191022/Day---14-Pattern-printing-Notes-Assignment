bf4c6867bca0f4eb8038611ba4d9c6ae35a7381e
Jyothi Ganapathiraju <96363260+Jyothi191022@users.noreply.github.com>
2025-10-29 16:25:51 +0530

    Inverted star pattern

---
+++ InvertedStarPattern.java
@@ -0,0 +1,18 @@
+package com.codegnan.arrays;
+
+public class InvertedStarPattern {
+
+	public static void main(String[] args) {
+		int n = 4;
+		for( int i = 0; i< n;i++) {
+			for (int j = 0; j < i; j++) {
+			System.out.print("  ");
+		}
+		    for (int k = i;k < n; k++) {
+			System.out.print("* ");
+		    }
+		    System.out.println();
+		}
+	}
+
+}
